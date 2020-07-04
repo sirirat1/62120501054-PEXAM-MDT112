97f98bab5985dd1697da77814ef3b13dcb3fcd4f
sirirat1 <61343388+sirirat1@users.noreply.github.com>
2020-07-04 16:07:54 +0700

    Update 62120501054-PEXAM-MDT112.ino

--- 62120501054-PEXAM-MDT112.ino
+++ 62120501054-PEXAM-MDT112.ino
@@ -9,3 +9,87 @@ void loop()
 {
 
 }
+int buzzer = 8;
+void setup() 
+{
+  pinMode(buzzer, OUTPUT);
+   tone(buzzer, 900, 600);
+   delay(100);
+   tone(buzzer, 900, 600);
+  delay(100); // ข้อ 1
+}
+void loop()
+{
+}
+int buzzer = 8;
+void setup() 
+{
+   delay(400);
+   pinMode(buzzer, OUTPUT);
+   tone(buzzer, 600, 400);
+   delay(100);
+   tone(buzzer, 600, 400);
+   delay(100); // ข้อ 6
+}
+int buzzer = 8;
+void setup() 
+{
+   pinMode(buzzer, OUTPUT);
+   tone(buzzer, 900, 600);
+ delay(100);
+   tone(buzzer, 900, 600);
+   delay(100); // ข้อ 1
+}
+void loop()
+{
+}
+int buzzer = 8;
+void setup() 
+{
+   delay(400);
+   pinMode(buzzer, OUTPUT);
+   tone(buzzer, 600, 400);
+   delay(100);
+   tone(buzzer, 600, 400);
+   delay(100); // ข้อ 6
+}
+void loop()
+{
+}
+#include "LiquidCrystal_I2C.h"
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() 
+{
+    lcd.init();
+   lcd.backlight();
+  lcd.setCursor(6, 0);
+   lcd.print("Hello");
+    lcd.setCursor(7, 1);
+    lcd.print("MDT");
+   delay(1500);
+   lcd.clear(); // ข้อ 5
+}
+void loop() 
+{
+    
+}
+void loop()
+{
+}
+#include "LiquidCrystal_I2C.h"
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() 
+{
+    lcd.init();
+   lcd.backlight();
+  lcd.setCursor(6, 0);
+    lcd.print("Hello");
+    lcd.setCursor(7, 1);
+    lcd.print("MDT");
+   delay(1500);
+   lcd.clear(); // ข้อ 5
+}
+void loop() 
+{
+    
+}
